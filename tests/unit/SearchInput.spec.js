0c3017976a969047f549afa79c0fadeca68eef74
Bryan Yunis <bbyunis@gmail.com>
2019-05-10 13:53:02 -0600

    remove mocking from searchinput spec

--- tests/unit/SearchInput.spec.js
+++ tests/unit/SearchInput.spec.js
@@ -3,7 +3,6 @@ import Vuetify from "vuetify"
 import SearchInput from "../../src/components/SearchInput.vue"
 import Vue from "vue"
 Vue.use(Vuetify)
-import flushPromises from "flush-promises"
 
 describe("SearchInput", () => {
     let build
@@ -19,24 +18,5 @@ describe("SearchInput", () => {
         const { wrapper } = build()
         expect(wrapper.html()).toMatchSnapshot()
     })
-
-
-    // it("fetches book data from api", async () => { const { wrapper, button, input } = build()
-
-    //     console.log(wrapper.mocks)
-
-    //     input().setValue("hemmingway")
-    //     button().trigger('click')
-    //     await flushPromises()
-    //     expect(wrapper.emitted("set-results")).toBeTruthy()
-    // })
 })
 
-
-/*
-        const mockResponse = {}
-        const mockJsonPromise = Promise.resolve(mockResponse)
-        const mockFetchPromise = Promise.resolve({
-            json: () => mockJsonPromise
-        })
-        */
